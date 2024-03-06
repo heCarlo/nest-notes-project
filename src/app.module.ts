e1a2a62ef8ea732aa8c63f07dd81ae3d0bfb8399
heCarlo <carlosgfkp@outlook.com>
2024-03-06 17:38:34 -0300

    feat(app.module): added DatabaseModule to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './infra/http/modules/user/user.module';
+import { DatabaseModule } from './infra/database/prisma/database.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
