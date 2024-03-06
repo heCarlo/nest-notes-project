9d61946c25abac938bf6d0582d9b0c3299cc9459
heCarlo <carlosgfkp@outlook.com>
2024-03-06 17:43:19 -0300

    feat: added imports to user.module and deleted other files

--- src/infra/http/modules/user/user.module.ts
+++ src/infra/http/modules/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { CreateUserUseCase } from 'src/modules/user/useCases/createUserUseCase/createUserUseCase';
+import { DatabaseModule } from 'src/infra/database/prisma/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UserController],
   providers: [CreateUserUseCase],
 })
