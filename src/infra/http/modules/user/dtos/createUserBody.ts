82fbe764bb2198aca6ed6d6b206e7a692db0bb2a
heCarlo <carlosgfkp@outlook.com>
2024-03-07 13:30:04 -0300

    feat: added class-validator decorators

--- src/infra/http/modules/user/dtos/createUserBody.ts
+++ src/infra/http/modules/user/dtos/createUserBody.ts
@@ -1,8 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserBody {
-    
-    name: string;
-    
-    password: string;
-    
-    email: string;
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
 }
